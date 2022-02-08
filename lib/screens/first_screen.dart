167b4f04522b072f4709a9d6f16394e0762aaf06
mensur056 <mansur.sarxanov@gmail.com>
2022-02-08 15:28:27 +0400

    My Portfolio

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../constants.dart';
+import 'package:portfolio/constants.dart';
 
 class FirstScreen extends StatefulWidget {
   const FirstScreen({Key? key}) : super(key: key);
@@ -17,68 +16,83 @@ class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
-        title: Row(
-          children: const [
-            Text(
-              'Flutter <',
-              style: TextStyle(
-                  fontFamily: 'Quintessential',
-                  fontSize: 25,
-                  color: Colors.white),
+      backgroundColor: kPrimaryColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 35,
             ),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-              'Mensur',
-              style: TextStyle(
-                  fontFamily: 'Satisfy', fontSize: 30, color: Colors.white),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-              '> Flutter',
-              style: TextStyle(
-                  fontFamily: 'Quintessential',
-                  fontSize: 25,
-                  color: Colors.white),
-            ),
-            Spacer(),
-            TextButton(
-              onPressed: null,
-              child: Text(
-                'Home',
-                style: TextStyle(fontSize: 25, color: Colors.white),
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15),
+              child: Row(
+                children: const [
+                  Text(
+                    'Flutter <',
+                    style: TextStyle(
+                        fontFamily: 'Quintessential',
+                        fontSize: 25,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Mensur',
+                    style: TextStyle(
+                        fontFamily: 'Satisfy',
+                        fontSize: 30,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    '> Flutter',
+                    style: TextStyle(
+                        fontFamily: 'Quintessential',
+                        fontSize: 25,
+                        color: Colors.white),
+                  ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: null,
+                    child: Text(
+                      'Home',
+                      style: TextStyle(fontSize: 25, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  TextButton(
+                    onPressed: null,
+                    child: Text(
+                      'Project',
+                      style: TextStyle(fontSize: 25, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  TextButton(
+                    onPressed: null,
+                    child: Text(
+                      'Contact',
+                      style: TextStyle(fontSize: 25, color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(
-              width: 10,
-            ),
-            TextButton(
-              onPressed: null,
-              child: Text(
-                'Project',
-                style: TextStyle(fontSize: 25, color: Colors.white),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            TextButton(
-              onPressed: null,
-              child: Text(
-                'Contact',
-                style: TextStyle(fontSize: 25, color: Colors.white),
-              ),
-            ),
+            const Text(
+              'Welcome To My Portfolio',
+              style:
+                  TextStyle(fontFamily: 'Quicksand', color: Colors.white,fontSize: 50),
+            )
           ],
         ),
       ),
-      backgroundColor: Colors.black,
-      body: SafeArea(child: Container()),
     );
   }
 }
