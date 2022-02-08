8cd4cc76db62a1d506c765c3fc71f2534fe0004c
mensur056 <mansur.sarxanov@gmail.com>
2022-02-08 15:14:10 +0400

    My Portfolio

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -23,21 +23,46 @@ class _FirstScreenState extends State<FirstScreen> {
           children: const [
             Text(
               'Flutter <',
-              style: TextStyle(fontFamily: 'Quintessential',fontSize:25,color:Colors.white),
+              style: TextStyle(
+                  fontFamily: 'Quintessential',
+                  fontSize: 25,
+                  color: Colors.white),
             ),
             SizedBox(
               width: 10,
             ),
             Text(
               'Mensur',
-              style: TextStyle(fontFamily: 'Satisfy',fontSize:30,color:Colors.white),
+              style: TextStyle(
+                  fontFamily: 'Satisfy', fontSize: 30, color: Colors.white),
+            ),
+            SizedBox(
+              width: 10,
+            ),Text(
+              '> Flutter',
+              style: TextStyle(
+                  fontFamily: 'Quintessential',
+                  fontSize: 25,
+                  color: Colors.white),
+            ),
+
+            Spacer(),
+            Text(
+              'Home',
+              style: TextStyle(fontSize: 25, color: Colors.white),
             ),
             SizedBox(
               width: 10,
             ),
             Text(
-              '> Flutter',
-              style: TextStyle(fontFamily: 'Quintessential',fontSize:25,color:Colors.white),
+              'Project',
+              style: TextStyle(fontSize: 25, color: Colors.white),
+            ),SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Contact',
+              style: TextStyle(fontSize: 25, color: Colors.white),
             ),
           ],
         ),
