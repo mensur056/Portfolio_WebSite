30ec640624aa573a0ef2a19ed927df847f1e88aa
mensur056 <mansur.sarxanov@gmail.com>
2022-02-08 14:16:08 +0400

    My Portfolio

---
+++ lib/screens/first_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
