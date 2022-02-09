c1526a65874b902f945164c42c7a801faad3c0eb
mensur056 <mansur.sarxanov@gmail.com>
2022-02-09 10:51:14 +0400

    My Portfolio

---
+++ lib/screens/knowledge_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class KnowledgeScreen extends StatelessWidget {
+  const KnowledgeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text(
+          'Welcaome',
+          style: TextStyle(color: Colors.white),
+        )
+      ],
+    );
+  }
+}
